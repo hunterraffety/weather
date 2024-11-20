3ff39c2b856c2f5c2b2b036b6e149d763611d1ff
Hunter Raffety <26361513+hunterraffety@users.noreply.github.com>
2024-11-20 11:41:18 -0500

    feat: integrate form, bring into page, testing api functionality

---
+++ src/app/components/InputForm.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+  city: string
+}
+
+type InputFormProps = {
+  onFetchWeather?: (params: { city: string }) => void
+}
+
+export default function InputForm({ onFetchWeather }: InputFormProps) {
+  console.log(onFetchWeather, '*** verify passing')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>()
+  console.log(errors, 'errors ***')
+  const onSubmit = (data: FormData) => {
+    console.log(data, 'data ***')
+    onFetchWeather?.({ city: data.city })
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-4/5 max-w-md bg-white p-8 rounded-lg shadow-lg space-y-6"
+    >
+      <h2 className="text-3xl font-semibold text-gray-800">Get Weather</h2>
+      <div>
+        <label className="block text-gray-600 text-sm">City Name</label>
+        <input
+          type="text"
+          // register w hook form -- validations
+          {...register('city', { required: 'City name is required' })}
+          className="w-full mt-2 px-4 py-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        {errors.city && (
+          <p className="text-red-500 text-sm mt-1">{errors.city.message}</p>
+        )}
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-blue-500 text-white py-3 rounded-md text-lg font-semibold hover:bg-blue-600 transition"
+      >
+        Fetch Weather
+      </button>
+    </form>
+  )
+}
